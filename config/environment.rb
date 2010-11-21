72fec3852a707344bcd56cade06c379f3bc316ad
Evan R. Murphy <evanrmurphy@gmail.com>
2010-11-20 18:48:14 -0600

    Make multi-page template (now let's see if it works)

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 JqueryMobileApp::Application.initialize!
+
+# Haml should use double quotes for tag attributes
+Haml::Template.options[:attr_wrapper] = '"'
